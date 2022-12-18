7852573ae43f689659ecd140caa0e554d102e5b0
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-12-18 02:44:10 +0100

    bottom

---
+++ cogs/bottom.py
@@ -0,0 +1,174 @@
+import discord
+from discord.ext import commands, tasks
+from discord.errors import Forbidden
+from discord.ext.commands import (
+    CommandNotFound,
+    BadArgument,
+    MissingRequiredArgument,
+    CommandOnCooldown,
+)
+import random
+
+import requests
+import re
+
+IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument)
+
+class events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_error(self, err, *args, **kwargs):
+        await self.bot.get_channel(984577196616216616).send(f"{err}")
+        
+        if err == "on_command_error":
+            await args[0].send("Sorry, something unexpected went wrong.")
+            # raise
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, exc):
+        if any([isinstance(exc, error) for error in IGNORE_EXCEPTIONS]):
+            # await ctx.send("Sorry, I couldn't find that command")
+            pass
+
+        elif isinstance(exc, MissingRequiredArgument):
+            await ctx.send(
+                f"One or more of the required arguments are missing, perhaps the help command could help you out? `{ctx.prefix}help {ctx.command}`"
+            )
+
+        elif isinstance(exc, CommandOnCooldown):
+            await ctx.send(
+                f"That command is on cooldown. Please try again in {exc.retry_after:,.2f} seconds.",
+                delete_after=(exc.retry_after*1.05 + 0.7),
+            )
+
+        #      elif isinstance(exc.original, HTTPException):
+        #          await ctx.send("Unable to send message.")
+
+        elif hasattr(exc, "original"):
+            # raise exc  # .original
+
+            if isinstance(exc.original, Forbidden):
+                await ctx.send("I do not have the permission to do that.")
+
+            else:
+                raise exc.original
+
+        else:
+            raise exc
+
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.author.bot:
+            return
+
+        if ctx.content == "🥺":
+            if random.randint(0, 80) != 2:
+                await ctx.reply("What the fuck is 🥺 use your words I don't speak bottom")
+                return
+            
+            await ctx.reply(file=discord.File("bottom.png"))
+    
+    @commands.command(name="milf")
+    async def milfCommand(self, ctx):
+        if not ctx.channel.is_nsfw():
+            return await ctx.reply("please use this command in a NSFW channel")
+        
+        await self.send_reddit(ctx.channel.id, "milf", True, 50)
+    
+    @commands.command(
+        name="reddit",
+        aliases=["red", "r/", "rslash", "r"],
+        brief="get a random reddit post from the specified subreddit",
+    )
+    async def reddit_search(self, ctx, search=None):
+        if not ctx.channel.is_nsfw():
+            return await ctx.reply("please use this command in a NSFW channel")
+        
+        await ctx.channel.typing()
+        if search == None:
+            search = random.choice([
+                "milf",
+                "milfs",
+                "rule34",
+                "collegesluts",
+                "collegeamateurs",
+                "maturemilf",
+                "nsfw_snapchat",
+                "cellshots",
+                "selfpix",
+                "girlswithiphones",
+                "freshfromtheshower",
+                "xposing",
+                "blonde",
+                "girlswithglasses",
+                "seethru",
+                "barelyclothed",
+                "bottomless",
+                "nopanties",
+                "tightdresses",
+                "asianhotties",
+                "juicyasians",
+                "nsfw_japan",
+                "gonewild",
+                "gonewildcurvy",
+                "altgonewild",
+                "publicflashing",
+                "RealPublicNudity",
+                "pegging",
+            ])
+        await self.send_reddit(ctx.channel.id, search, True, 50)
+    
+    async def send_reddit(self, channel, subreddit, imageRequired=False, limit=25):
+        if self.bot.is_ready():
+            try:
+                req = requests.get(
+                    f"http://reddit.com/r/{subreddit}/hot.json?limit={limit}",
+                    headers={"User-agent": "Chrome"},
+                )
+                json = req.json()
+                if "error" in json or json["data"]["after"] is None:
+                    await self.bot.get_channel(channel).send(f"an error occured, no r/{subreddit} for now :(")
+                    return
+
+                req_len = len(json["data"]["children"])
+                
+                if imageRequired:
+                    for i in range(req_len):
+                        post = json["data"]["children"][i]
+                        url = post["data"]["url"] # can be image or post link
+                        if re.match(r".*\.(jpg|png|gif)$", url):
+                            break
+                
+                else:
+                    rand = random.randrange(0, req_len)
+                    post = json["data"]["children"][rand]
+                    url = post["data"]["url"] # can be image or post link
+                    
+                              
+                title = post["data"]["title"]
+                author = "u/" + post["data"]["author"]
+                subreddit = post["data"]["subreddit_name_prefixed"]
+                link = "https://reddit.com" + post["data"]["permalink"]
+                if "selftext" in post["data"]:
+                    text = post["data"]["selftext"]  # may not exist
+                    if len(text) >= 2000:
+                        text = text[:2000].rsplit(" ", 1)[0] + " **-Snippet-**"
+                    embed = discord.Embed(title=title, description=text, url=link)
+                else:
+                    embed = discord.Embed(title=title, url=link)
+                
+                if re.match(r".*\.(jpg|png|gif)$", url):
+                    embed.set_image(url=url)
+
+                embed.set_footer(text=f"By {author} in {subreddit}")
+                
+                await self.bot.get_channel(channel).send(embed=embed)
+            
+            except Exception as e:
+                print(f"error in send_reddit: {e}")
+
+async def setup(bot):
+    await bot.add_cog(events(bot))
---
+++ main.py
@@ -0,0 +1,113 @@
+import os
+import glob
+import random
+import logging
+from time import time
+import json
+import asyncio
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+import discord
+from discord.ext import tasks, commands
+
+from dotenv import load_dotenv
+
+# Load dotenv file
+load_dotenv("keys.env")
+TOKEN = os.getenv("DISCORD")#_STABLE")
+
+# Load config file
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# Grab vars from config.json
+OWNER_IDS = config["OWNER_IDS"]
+APP_ID = config["APP_ID"]
+
+# Logging
+logging.basicConfig(
+    level=logging.INFO,
+    filename=f"logs/{time()}.log",
+    filemode="w",
+    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+)
+
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        prefixes = []
+        prefixes.append("🥺 ")
+        prefixes.append("🥺")
+        prefixes.append("mommy ")
+        prefixes.append("mommy")
+        
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(*prefixes),
+            intents=discord.Intents.all(),
+            owner_ids=OWNER_IDS,
+        )
+        self.synced = False
+    
+    async def sync_tree(self):
+        if not self.synced:
+            # await tree.sync(guild = discord.Object(id = 1016777760305320036))
+            # print("slash commands synced successfully!")
+            self.synced = True
+    
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+        await self.wait_until_ready()
+        print("Slash commands are now ready!")
+        
+        await self.sync_tree()
+        if not self.ready:
+            change_status_task.start()
+
+            guild_count = 0
+            for guild in self.guilds:
+                print(f"- {guild.id} (name: {guild.name})")
+                guild_count += 1
+
+            # Print the bot name, number of guilds, and number of shards.
+            print(f"{self.user} is in {guild_count} guild(s).\nwith {self.shard_count} shard(s)")
+
+            # Set the bot ready to True
+            self.ready = True
+
+
+bot = MyBot()
+tree = bot.tree
+
+# Remove default help command
+# bot.remove_command("help")
+# Set the ready status to False, so the bot knows it hasnt been initialized yet.
+bot.ready = False
+
+@tasks.loop(minutes = 1)
+async def change_status_task():
+    await bot.change_presence(
+        status=discord.Status.idle,
+        activity=discord.Activity(type=discord.ActivityType.watching, name="sauce"),
+    )
+
+async def load_cogs(bot):
+    print("Loading cogs...")
+    # loads cogs
+    for filename in glob.iglob("./cogs/**", recursive=True):
+        if filename.endswith('.py'):
+            filename = filename[2:].replace("/", ".") # goes from "./cogs/economy.py" to "cogs.economy.py"
+            await bot.load_extension(f'{filename[:-3]}') # removes the ".py" from the end of the filename, to make it into cogs.economy
+    
+
+async def main():
+    async with bot:
+        await load_cogs(bot)
+        await bot.start(TOKEN)
+
+asyncio.run(main())
